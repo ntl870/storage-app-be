22409268c2f321bc03cb6df6c94bcacdff4ac9ee
Long Nguyen <ntlong870@gmail.com>
2023-05-28 15:33:59 +0700

    initialy done

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,6 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true });
     await connectToDb();
 
-    // initialize graphql-upload middleware
-    // app.use(
-    //   graphqlUploadExpress({ maxFileSize: 100000000000000, maxFiles: 10000 }),
-    // );
-
     await app.listen(8000);
   } catch (err) {
     throw new Error(err);
