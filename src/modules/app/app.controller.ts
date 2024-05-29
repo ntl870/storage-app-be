3c32b499edec3ddc416e25902112871dca1bcf59
Long Nguyen <ntlong870@gmail.com>
2024-05-29 17:27:01 +0700

    update pipeline

--- src/modules/app/app.controller.ts
+++ src/modules/app/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get('/health')
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! again';
   }
 }
